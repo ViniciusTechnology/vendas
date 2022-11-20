d73a5e02d08f341a7a33489501c75b466ca7ff4d
vinicius1994 <vinicius.barreto994@gmail.com>
2022-11-20 07:47:32 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/vendas/dsvendas/entities/Sale.java
@@ -0,0 +1,75 @@
+package com.vendas.dsvendas.entities;
+
+import javax.persistence.*;
+import javax.xml.crypto.Data;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private Seller seller;
+
+    public Sale(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getVisited() {
+        return visited;
+    }
+
+    public void setVisited(Integer visited) {
+        this.visited = visited;
+    }
+
+    public Integer getDeals() {
+        return deals;
+    }
+
+    public void setDeals(Integer deals) {
+        this.deals = deals;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+}
