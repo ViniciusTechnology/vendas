22222bc8751f2f498aeb2d9619be5c659a962a1d
vinicius1994 <vinicius.barreto994@gmail.com>
2022-11-20 08:29:57 -0300

    Layers

---
+++ backend/src/main/java/com/vendas/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.vendas.dsvendas.repositories;
+
+import com.vendas.dsvendas.entities.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+}
---
+++ backend/src/main/java/com/vendas/dsvendas/services/SellerService.java
@@ -0,0 +1,22 @@
+package com.vendas.dsvendas.services;
+
+import com.vendas.dsvendas.dto.SellerDTO;
+import com.vendas.dsvendas.entities.Seller;
+import com.vendas.dsvendas.repositories.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SellerService {
+
+    @Autowired
+    SellerRepository repository;
+
+    public List<SellerDTO> findAll(){
+        List<Seller> result = repository.findAll();
+        return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+    }
+}
